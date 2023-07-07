18ba203285f9e7e6ac480eca43ffc59e4106e537
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-07-08 00:30:55 +0430

    added types folder and object.py

---
+++ balethon/types/object.py
@@ -0,0 +1,16 @@
+class Object:
+
+    def __init__(self):
+        pass
+
+    def to_dict(self):
+        return {}
+
+    def __str__(self):
+        return ""
+
+    def __repr__(self):
+        return ""
+
+    def __iter__(self):
+        return iter([])
