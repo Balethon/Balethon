be6548990d65890b210383ea51279511462de447
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-07-07 16:39:34 +0430

    added dispatcher file

---
+++ balethon/dispatcher.py
@@ -0,0 +1,14 @@
+class Dispatcher:
+
+    def __init__(self):
+        self.handlers = []
+
+    def add_handler(self, handler):
+        self.handlers.append(handler)
+
+    def remove_handler(self, handler):
+        self.handlers.remove(handler)
+
+    def dispatch(self, update):
+        for handler in self.handlers:
+            pass
