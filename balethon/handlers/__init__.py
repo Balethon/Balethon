434915575cfe74880038b7cce02d0374da3ea1ba
Abolfazl.A <107195131+abolX@users.noreply.github.com>
2023-07-08 15:06:03 +0330

    Update __init__.py

--- balethon/handlers/__init__.py
+++ balethon/handlers/__init__.py
@@ -1,3 +1,4 @@
 from .handler import Handler
 from .message_handler import MessageHandler
 from .callback_query_handler import CallbackQueryHandler
+from .edited_message_handler import EditedMessageHandler
