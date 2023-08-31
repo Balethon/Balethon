ab148e49a5b08788d68b3c13bd65db3f1de4b296
Abolfazl Amiri <107195131+abolX@users.noreply.github.com>
2023-09-01 01:41:29 +0330

    Update message.py
    added Message.edit_text

--- balethon/objects/media/message.py
+++ balethon/objects/media/message.py
@@ -48,3 +48,7 @@ class Message(Object):
     async def reply(self, text, reply_markup=None, client=None):
         client = client or self.client
         return await client.send_message(self.chat.id, text, reply_markup, self.id)
+
+    async def edit_text(self,  text, reply_markup=None, client=None):
+        client = client or self.client
+        return await client.edit_message_text(self.chat.id, self.id, text, reply_markup)
