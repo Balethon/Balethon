c52e5c60b43cd94f0c5e52d652c8275dd683c6ef
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-10-15 17:44:52 +0330

    Added test units

--- tests/conditions.py
+++ tests/conditions.py
@@ -1,5 +1,5 @@
 from balethon import Client
-from balethon.conditions import *
+from balethon import conditions
 
 from config import TOKEN
 
@@ -11,127 +11,132 @@ async def main(_, message):
     print(f"{message.author.full_name}: {message.text}")
 
 
-@bot.on_message(all)
+@bot.on_message(conditions.all)
 async def test_all(_, __):
     print("This message falls under the condition of ALL")
 
 
-@bot.on_message(forward)
+@bot.on_message(conditions.forward)
 async def test_forward(_, __):
     print("This message falls under the condition of FORWARD")
 
 
-@bot.on_message(reply)
+@bot.on_message(conditions.reply)
 async def test_reply(_, __):
     print("This message falls under the condition of REPLY")
 
 
-@bot.on_message(text)
+@bot.on_message(conditions.text)
 async def test_text(_, __):
     print("This message falls under the condition of TEXT")
 
 
-@bot.on_message(entities)
+@bot.on_message(conditions.entities)
 async def test_entities(_, __):
     print("This message falls under the condition of ENTITIES")
 
 
-@bot.on_message(document)
+@bot.on_message(conditions.document)
 async def test_document(_, __):
     print("This message falls under the condition of DOCUMENT")
 
 
-@bot.on_message(photo)
+@bot.on_message(conditions.photo)
 async def test_photo(_, __):
     print("This message falls under the condition of PHOTO")
 
 
-@bot.on_message(video)
+@bot.on_message(conditions.video)
 async def test_video(_, __):
     print("This message falls under the condition of VIDEO")
 
 
-@bot.on_message(voice)
+@bot.on_message(conditions.voice)
 async def test_voice(_, __):
     print("This message falls under the condition of VOICE")
 
 
-@bot.on_message(caption)
+@bot.on_message(conditions.caption)
 async def test_caption(_, __):
     print("This message falls under the condition of CAPTION")
 
 
-@bot.on_message(contact)
+@bot.on_message(conditions.contact)
 async def test_contact(_, __):
     print("This message falls under the condition of CONTACT")
 
 
-@bot.on_message(location)
+@bot.on_message(conditions.location)
 async def test_location(_, __):
     print("This message falls under the condition of LOCATION")
 
 
-@bot.on_message(new_chat_members)
+@bot.on_message(conditions.new_chat_members)
 async def test_new_chat_members(_, __):
     print("This message falls under the condition of NEW CHAT MEMBERS")
 
 
-@bot.on_message(left_chat_member)
+@bot.on_message(conditions.left_chat_member)
 async def test_left_chat_member(_, __):
     print("This message falls under the condition of LEFT CHAT MEMBER")
 
 
-@bot.on_message(new_chat_title)
+@bot.on_message(conditions.new_chat_title)
 async def test_new_chat_title(_, __):
     print("This message falls under the condition of NEW CHAT TITLE")
 
 
-@bot.on_message(new_chat_photo)
+@bot.on_message(conditions.new_chat_photo)
 async def test_new_chat_photo(_, __):
     print("This message falls under the condition of NEW CHAT PHOTO")
 
 
-@bot.on_message(delete_chat_photo)
+@bot.on_message(conditions.delete_chat_photo)
 async def test_delete_chat_photo(_, __):
     print("This message falls under the condition of DELETE CHAT PHOTO")
 
 
-@bot.on_message(group_chat_created)
+@bot.on_message(conditions.group_chat_created)
 async def test_group_chat_created(_, __):
     print("This message falls under the condition of GROUP CHAT CREATED")
 
 
-@bot.on_message(supergroup_chat_created)
+@bot.on_message(conditions.supergroup_chat_created)
 async def test_supergroup_chat_created(_, __):
     print("This message falls under the condition of SUPERGROUP CHAT CREATED")
 
 
-@bot.on_message(channel_chat_created)
+@bot.on_message(conditions.channel_chat_created)
 async def test_channel_chat_created(_, __):
     print("This message falls under the condition of CHANNEL CHAT CREATED")
 
 
-@bot.on_message(pinned_message)
+@bot.on_message(conditions.pinned_message)
 async def test_pinned_message(_, __):
     print("This message falls under the condition of PINNED MESSAGE")
 
 
-@bot.on_message(invoice)
+@bot.on_message(conditions.invoice)
 async def test_invoice(_, __):
     print("This message falls under the condition of INVOICE")
 
 
-@bot.on_message(media)
+@bot.on_message(conditions.media)
 async def test_media(_, __):
     print("This message falls under the condition of MEDIA")
 
 
-@bot.on_message(private)
+@bot.on_message(conditions.Command("command"))
+async def test_command(_, __):
+    print("This message falls under the condition of COMMAND")
+
+
+@bot.on_message(conditions.private)
 async def test_private(_, __):
     print("This message falls under the condition of PRIVATE")
 
 
-@bot.on_message(Regex("test"))
+@bot.on_message(conditions.Regex("test"))
 async def test_private(_, __):
     print("This message falls under the condition of REGEX")
 
--- tests/event_handlers.py
+++ tests/event_handlers.py
@@ -5,6 +5,21 @@ from config import TOKEN
 bot = Client(TOKEN)
 
 
+@bot.on_event()
+async def show_event(client, event):
+    print(f"EVENT - {event}")
+
+
+@bot.on_connect()
+async def connection(client):
+    print("CONNECTED")
+
+
+@bot.on_update()
+async def show_update(client, update):
+    print(f"UPDATE - {update.author.full_name}: {update}")
+
+
 @bot.on_command()
 async def show_command(client, message):
     print(f"COMMAND - {message.author.full_name}: {message.text}")
@@ -18,6 +33,26 @@ async def show_message(client, message):
 @bot.on_callback_query()
 async def show_callback_query(client, callback_query):
     print(f"CALLBACK QUERY - {callback_query.author.full_name}: {callback_query.data}")
+
+
+@bot.on_pre_checkout_query()
+async def show_pre_checkout_query(client, pre_checkout_query):
+    print(f"PRE CHECKOUT QUERY - {pre_checkout_query.author.full_name}: {pre_checkout_query}")
+
+
+@bot.on_shipping_query()
+async def show_shipping_query(client, shipping_query):
+    print(f"SHIPPING QUERY {shipping_query.author.full_name}: {shipping_query}")
+
+
+@bot.on_error()
+async def show_error(client, error):
+    print(f"ERROR - {error}")
+
+
+@bot.on_disconnect()
+async def disconnection(client):
+    print("DISCONNECTED")
 
 
 if __name__ == "__main__":
