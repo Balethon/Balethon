e59004e21925990ffcd50cfa7c225530582b5926
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-10-31 23:03:45 +0330

    Deleted unready sticker methods

--- balethon/client/stickers/__init__.py
+++ balethon/client/stickers/__init__.py
@@ -1,8 +1,5 @@
-from .add_sticker_to_set import AddStickerToSet
-from .create_new_sticker_set import CreateNewStickerSet
-from .delete_sticker_from_set import DeleteStickerFromSet
 from .get_sticker_set import GetStickerSet
 
 
-class Stickers(AddStickerToSet, CreateNewStickerSet, DeleteStickerFromSet, GetStickerSet):
+class Stickers(GetStickerSet):
     pass
