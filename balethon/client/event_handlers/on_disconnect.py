5935b6de70bec349c5bbb0055584a6820f0e23dc
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-11-03 18:05:59 +0330

    Fixed on_disconnect.py

--- balethon/client/event_handlers/on_disconnect.py
+++ balethon/client/event_handlers/on_disconnect.py
@@ -4,7 +4,7 @@ from ...event_handlers import DisconnectHandler
 
 class OnDisconnect:
 
-    def on_connect(
+    def on_disconnect(
             self: "balethon.Client"
     ):
         return self.add_event_handler(DisconnectHandler)
