825e7b357c5396a5bfd608c081d21569eb8c11e1
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-11-14 22:36:55 +0330

    Updated the ChatMember Object

--- balethon/objects/chat_member.py
+++ balethon/objects/chat_member.py
@@ -6,13 +6,20 @@ class ChatMember(Object):
 
     def __init__(
             self,
+            is_member: bool = None,
+            can_send_media: bool = None,
+            can_send_gif_stickers: bool = None,
+            can_forward_message_from: bool = None,
+            can_send_gift_packet: bool = None,
+            can_send_link_message: bool = None,
+            can_send_forwarded_message: bool = None,
+            can_kick_user: bool = None,
+            can_send_message: bool = None,
+            can_see_members: bool = None,
+            can_add_story: bool = None,
             user: "objects.User" = None,
             status: str = None,
-            until_date: "objects.Date" = None,
-            can_be_edited: bool = None,
             can_change_info: bool = None,
-            can_post_messages: bool = None,
-            can_edit_messages: bool = None,
             can_delete_messages: bool = None,
             can_invite_users: bool = None,
             can_restrict_members: bool = None,
@@ -20,18 +27,23 @@ class ChatMember(Object):
             can_promote_members: bool = None,
             can_send_messages: bool = None,
             can_send_media_messages: bool = None,
-            can_send_other_messages: bool = None,
-            can_add_web_page_previews: bool = None,
             **kwargs
     ):
         super().__init__(**kwargs)
+        self.is_member: bool = is_member
+        self.can_send_media: bool = can_send_media
+        self.can_send_gif_stickers: bool = can_send_gif_stickers
+        self.can_forward_message_from: bool = can_forward_message_from
+        self.can_send_gift_packet: bool = can_send_gift_packet
+        self.can_send_link_message: bool = can_send_link_message
+        self.can_send_forwarded_message: bool = can_send_forwarded_message
+        self.can_kick_user: bool = can_kick_user
+        self.can_send_message: bool = can_send_message
+        self.can_see_members: bool = can_see_members
+        self.can_add_story: bool = can_add_story
         self.user: "objects.User" = user
         self.status: str = status
-        self.until_date: "objects.Date" = until_date
-        self.can_be_edited: bool = can_be_edited
         self.can_change_info: bool = can_change_info
-        self.can_post_messages: bool = can_post_messages
-        self.can_edit_messages: bool = can_edit_messages
         self.can_delete_messages: bool = can_delete_messages
         self.can_invite_users: bool = can_invite_users
         self.can_restrict_members: bool = can_restrict_members
@@ -39,5 +51,3 @@ class ChatMember(Object):
         self.can_promote_members: bool = can_promote_members
         self.can_send_messages: bool = can_send_messages
         self.can_send_media_messages: bool = can_send_media_messages
-        self.can_send_other_messages: bool = can_send_other_messages
-        self.can_add_web_page_previews: bool = can_add_web_page_previews
