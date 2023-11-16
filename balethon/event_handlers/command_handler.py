01f4bcfdbf818c36acf5cc896403c0369cb7105d
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2023-11-16 15:57:36 +0330

    Renamed some conditions

--- balethon/event_handlers/command_handler.py
+++ balethon/event_handlers/command_handler.py
@@ -1,5 +1,5 @@
 from .message_handler import MessageHandler
-from ..conditions import text, Command
+from ..conditions import text, command
 from ..objects import Message
 
 
@@ -9,7 +9,7 @@ class CommandHandler(MessageHandler):
     def __init__(self, callback, condition=None, name=None):
         if name is None:
             name = callback.__name__
-        command_condition = Command(name)
+        command_condition = command(name)
         if condition is None:
             condition = text & command_condition
         else:
