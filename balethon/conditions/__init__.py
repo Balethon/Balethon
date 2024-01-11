e7414de579cb25ec7c59ea9ce1ac10e8267ec5cc
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-01-11 15:16:58 +0330

    Renamed files inside `balethon.conditions`

--- balethon/conditions/__init__.py
+++ balethon/conditions/__init__.py
@@ -1,33 +1,33 @@
 from .condition import Condition, AllCondition, AnyCondition, NotCondition
-from .all_condition import all
-from .forward_condition import forward
-from .reply_condition import reply
-from .text_condition import text
-from .entities_condition import entities
-from .document_condition import document
-from .photo_condition import photo
-from .video_condition import video
-from .voice_condition import voice
-from .caption_condition import caption
-from .contact_condition import contact
-from .location_condition import location
-from .new_chat_members_condition import new_chat_members
-from .left_chat_member_condition import left_chat_member
-from .new_chat_title_condition import new_chat_title
-from .new_chat_photo_condition import new_chat_photo
-from .delete_chat_photo_condition import delete_chat_photo
-from .group_chat_created_condition import group_chat_created
-from .supergroup_chat_created_condition import supergroup_chat_created
-from .channel_chat_created_condition import channel_chat_created
-from .pinned_message_condition import pinned_message
-from .invoice_condition import invoice
-from .media_condition import media
-from .command_condition import Command as command
-from .private_condition import private
-from .group_condition import group
-from .regex_condition import Regex as regex
-from .at_state_condition import AtState as at_state
-from .is_instance_condition import IsInstance as is_instance
-from .chat_condition import Chat as chat
-from .is_joined_condition import IsJoined as is_joined
-from .author_condition import Author as author
+from .all import all
+from .forward import forward
+from .reply import reply
+from .text import text
+from .entities import entities
+from .document import document
+from .photo import photo
+from .video import video
+from .voice import voice
+from .caption import caption
+from .contact import contact
+from .location import location
+from .new_chat_members import new_chat_members
+from .left_chat_member import left_chat_member
+from .new_chat_title import new_chat_title
+from .new_chat_photo import new_chat_photo
+from .delete_chat_photo import delete_chat_photo
+from .group_chat_created import group_chat_created
+from .supergroup_chat_created import supergroup_chat_created
+from .channel_chat_created import channel_chat_created
+from .pinned_message import pinned_message
+from .invoice import invoice
+from .media import media
+from .command import Command as command
+from .private import private
+from .group import group
+from .regex import Regex as regex
+from .at_state import AtState as at_state
+from .is_instance import IsInstance as is_instance
+from .chat import Chat as chat
+from .is_joined import IsJoined as is_joined
+from .author import Author as author
