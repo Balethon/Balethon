8c7e250060dfb84b78ae6b445e59764f93fe3b7c
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-01-11 13:54:45 +0330

    Added the 'reply' methods to the `Message` Object

--- balethon/objects/message.py
+++ balethon/objects/message.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Union, BinaryIO
 
-from . import Object
+from . import Object, InputMedia
 from balethon import objects
 
 
@@ -82,23 +82,135 @@ class Message(Object):
         self.invoice: "objects.Invoice" = invoice
         self.successful_payment: "objects.SuccessfulPayment" = successful_payment
 
-    async def reply(self, text, reply_markup=None, client=None):
+    async def reply_animation(
+            self,
+            animation: Union[str, bytes, BinaryIO, InputMedia],
+            duration: int = None,
+            width: int = None,
+            height: int = None,
+            caption: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_animation(self.chat.id, animation, duration, width, height, caption, self.id)
+
+    async def reply_audio(
+            self,
+            audio: Union[str, bytes, BinaryIO, InputMedia],
+            caption: str = None,
+            duration: int = None,
+            title: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_audio(self.chat.id, audio, caption, duration, title, self.id)
+
+    async def reply_contact(
+            self,
+            phone_number: str,
+            first_name: str,
+            last_name: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_contact(self.chat.id, phone_number, first_name, last_name, self.id)
+
+    async def reply_document(
+            self,
+            document: Union[str, bytes, BinaryIO, InputMedia],
+            caption: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_document(self.chat.id, document, caption, self.id)
+
+    async def reply_location(
+            self,
+            latitude: int,
+            longitude: int,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_location(self.chat.id, longitude, latitude, self.id)
+
+    async def reply_media_group(
+            self,
+            media: List["objects.InputMedia"],
+            client=None
+    ):
+        client = client or self.client
+        await client.send_media_group(self.chat.id, media)
+
+    async def reply_photo(
+            self,
+            photo: Union[str, bytes, BinaryIO, InputMedia],
+            caption: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_photo(self.chat.id, photo, caption, self.id)
+
+    async def reply_video(
+            self,
+            video: Union[str, bytes, BinaryIO, InputMedia],
+            duration: int = None,
+            width: int = None,
+            height: int = None,
+            caption: str = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_video(self.chat.id, video, duration, width, height, caption)
+
+    async def reply_voice(
+            self,
+            voice: Union[str, bytes, BinaryIO, InputMedia],
+            caption: str = None,
+            duration: int = None,
+            client=None
+    ):
+        client = client or self.client
+        await client.send_voice(self.chat.id, voice, caption, duration, self.id)
+
+    async def reply(
+            self,
+            text: str,
+            reply_markup: "objects.ReplyMarkup" = None,
+            client=None
+    ):
         client = client or self.client
         return await client.send_message(self.chat.id, text, reply_markup, self.id)
 
-    async def edit_text(self,  text, reply_markup=None, client=None):
+    async def edit_text(
+            self,
+            text,
+            reply_markup=None,
+            client=None
+    ):
         client = client or self.client
         return await client.edit_message_text(self.chat.id, self.id, text, reply_markup)
 
-    async def delete(self, client=None):
+    async def delete(
+            self,
+            client=None
+    ):
         client = client or self.client
         return await client.delete_message(self.chat.id, self.id)
 
-    async def forward(self, chat_id, client=None):
+    async def forward(
+            self,
+            chat_id,
+            client=None
+    ):
         client = client or self.client
         return await client.forward_message(chat_id, self.chat.id, self.id)
 
-    async def copy(self, chat_id, reply_to_message_id=None, client=None):
+    async def copy(
+            self,
+            chat_id,
+            reply_to_message_id=None,
+            client=None
+    ):
         client = client or self.client
         if self.text:
             return await client.send_message(
