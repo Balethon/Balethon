3638b42c96ed2d65e684681660261f168575d21f
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-04-29 21:12:06 +0330

    Changed Document.file_name to Document.name

--- balethon/objects/document.py
+++ balethon/objects/document.py
@@ -5,14 +5,15 @@ from balethon import objects
 class Document(Object):
     attribute_names = [
         ("id", "file_id"),
-        ("size", "file_size")
+        ("size", "file_size"),
+        ("name", "file_name")
     ]
 
     def __init__(
             self,
             id: str = None,
             thumb: "objects.Photo" = None,
-            file_name: str = None,
+            name: str = None,
             mime_type: str = None,
             size: int = None,
             **kwargs
@@ -20,6 +21,6 @@ class Document(Object):
         super().__init__(**kwargs)
         self.id: str = id
         self.thumb: None = thumb
-        self.file_name: str = file_name
+        self.name: str = name
         self.mime_type: str = mime_type
         self.size: int = size
