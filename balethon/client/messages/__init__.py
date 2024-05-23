a4135a3c8967cc619ba9b54c244877631a01c97b
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-05-23 23:26:19 +0330

    Refactored some code

--- balethon/client/messages/__init__.py
+++ balethon/client/messages/__init__.py
@@ -2,7 +2,8 @@ from .send_message import SendMessage
 from .edit_message_text import EditMessageText
 from .forward_message import ForwardMessage
 from .delete_message import DeleteMessage
+from .copy_message import CopyMessage
 
 
-class Messages(SendMessage, EditMessageText, ForwardMessage, DeleteMessage):
+class Messages(SendMessage, EditMessageText, ForwardMessage, DeleteMessage, CopyMessage):
     pass
--- balethon/client/messages/forward_message.py
+++ balethon/client/messages/forward_message.py
@@ -9,10 +9,11 @@ class ForwardMessage:
     async def forward_message(
             self: "balethon.Client",
             chat_id: Union[int, str],
-            from_chat_id: int,
+            from_chat_id: Union[int, str],
             message_id: int
     ) -> Message:
         chat_id = await self.resolve_peer_id(chat_id)
+        from_chat_id = await self.resolve_peer_id(from_chat_id)
         data = locals()
         del data["self"]
         result = await self.execute("post", "forwardMessage", **data)
