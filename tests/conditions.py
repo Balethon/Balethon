ec07feda6f8a1cbb2de88196baea52cdf205b481
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-08-05 20:43:16 +0330

    Updated tests

--- tests/conditions.py
+++ tests/conditions.py
@@ -11,147 +11,147 @@ async def main(_, message):
     print(f"{message.author.full_name}: {message.text}")
 
 
-@bot.on_message(conditions.all, "all")
+@bot.on_message(conditions.all)
 async def test_all(_, __):
     print("This message falls under the condition of ALL")
 
 
-@bot.on_message(conditions.forward, "forward")
+@bot.on_message(conditions.forward)
 async def test_forward(_, __):
     print("This message falls under the condition of FORWARD")
 
 
-@bot.on_message(conditions.reply, "reply")
+@bot.on_message(conditions.reply)
 async def test_reply(_, __):
     print("This message falls under the condition of REPLY")
 
 
-@bot.on_message(conditions.text, "text")
+@bot.on_message(conditions.text)
 async def test_text(_, __):
     print("This message falls under the condition of TEXT")
 
 
-@bot.on_message(conditions.entities, "entities")
+@bot.on_message(conditions.entities)
 async def test_entities(_, __):
     print("This message falls under the condition of ENTITIES")
 
 
-@bot.on_message(conditions.document, "document")
+@bot.on_message(conditions.document)
 async def test_document(_, __):
     print("This message falls under the condition of DOCUMENT")
 
 
-@bot.on_message(conditions.photo, "photo")
+@bot.on_message(conditions.photo)
 async def test_photo(_, __):
     print("This message falls under the condition of PHOTO")
 
 
-@bot.on_message(conditions.video, "video")
+@bot.on_message(conditions.video)
 async def test_video(_, __):
     print("This message falls under the condition of VIDEO")
 
 
-@bot.on_message(conditions.voice, "voice")
+@bot.on_message(conditions.voice)
 async def test_voice(_, __):
     print("This message falls under the condition of VOICE")
 
 
-@bot.on_message(conditions.caption, "caption")
+@bot.on_message(conditions.caption)
 async def test_caption(_, __):
     print("This message falls under the condition of CAPTION")
 
 
-@bot.on_message(conditions.contact, "contact")
+@bot.on_message(conditions.contact)
 async def test_contact(_, __):
     print("This message falls under the condition of CONTACT")
 
 
-@bot.on_message(conditions.location, "location")
+@bot.on_message(conditions.location)
 async def test_location(_, __):
     print("This message falls under the condition of LOCATION")
 
 
-@bot.on_message(conditions.new_chat_members, "new_chat_members")
+@bot.on_message(conditions.new_chat_members)
 async def test_new_chat_members(_, __):
     print("This message falls under the condition of NEW CHAT MEMBERS")
 
 
-@bot.on_message(conditions.left_chat_member, "left_chat_member")
+@bot.on_message(conditions.left_chat_member)
 async def test_left_chat_member(_, __):
     print("This message falls under the condition of LEFT CHAT MEMBER")
 
 
-@bot.on_message(conditions.new_chat_title, "new_chat_title")
+@bot.on_message(conditions.new_chat_title)
 async def test_new_chat_title(_, __):
     print("This message falls under the condition of NEW CHAT TITLE")
 
 
-@bot.on_message(conditions.new_chat_photo, "new_chat_photo")
+@bot.on_message(conditions.new_chat_photo)
 async def test_new_chat_photo(_, __):
     print("This message falls under the condition of NEW CHAT PHOTO")
 
 
-@bot.on_message(conditions.delete_chat_photo, "delete_chat_photo")
+@bot.on_message(conditions.delete_chat_photo)
 async def test_delete_chat_photo(_, __):
     print("This message falls under the condition of DELETE CHAT PHOTO")
 
 
-@bot.on_message(conditions.group_chat_created, "group_chat_created")
+@bot.on_message(conditions.group_chat_created)
 async def test_group_chat_created(_, __):
     print("This message falls under the condition of GROUP CHAT CREATED")
 
 
-@bot.on_message(conditions.supergroup_chat_created, "supergroup_chat_created")
+@bot.on_message(conditions.supergroup_chat_created)
 async def test_supergroup_chat_created(_, __):
     print("This message falls under the condition of SUPERGROUP CHAT CREATED")
 
 
-@bot.on_message(conditions.channel_chat_created, "channel_chat_created")
+@bot.on_message(conditions.channel_chat_created)
 async def test_channel_chat_created(_, __):
     print("This message falls under the condition of CHANNEL CHAT CREATED")
 
 
-@bot.on_message(conditions.pinned_message, "pinned_message")
+@bot.on_message(conditions.pinned_message)
 async def test_pinned_message(_, __):
     print("This message falls under the condition of PINNED MESSAGE")
 
 
-@bot.on_message(conditions.invoice, "invoice")
+@bot.on_message(conditions.invoice)
 async def test_invoice(_, __):
     print("This message falls under the condition of INVOICE")
 
 
-@bot.on_message(conditions.media, "media")
+@bot.on_message(conditions.media)
 async def test_media(_, __):
     print("This message falls under the condition of MEDIA")
 
 
-@bot.on_message(conditions.command("command"), "command")
+@bot.on_message(conditions.command("command"))
 async def test_command(_, __):
     print("This message falls under the condition of COMMAND")
 
 
-@bot.on_message(conditions.private, "private")
+@bot.on_message(conditions.private)
 async def test_private(_, __):
     print("This message falls under the condition of PRIVATE")
 
 
-@bot.on_message(conditions.group, "group")
+@bot.on_message(conditions.group)
 async def test_group(_, __):
     print("This message falls under the condition of GROUP")
 
 
-@bot.on_message(conditions.regex("test"), "regex")
+@bot.on_message(conditions.regex("test"))
 async def test_private(_, __):
     print("This message falls under the condition of REGEX")
 
 
-@bot.on_message(conditions.chat(int(config.USER_ID)), "chat")
+@bot.on_message(conditions.chat(int(config.USER_ID)))
 async def test_chat(_, __):
     print("This message falls under the condition of CHAT")
 
 
-@bot.on_message(conditions.is_joined(config.CHAT_ID), "is_joined")
+@bot.on_message(conditions.is_joined(config.CHAT_ID))
 async def test_is_joined(_, __):
     print("This message falls under the condition of IS JOINED")
 
--- tests/methods.py
+++ tests/methods.py
@@ -1,11 +1,13 @@
 from asyncio import run, sleep
 
 from balethon import Client
+from balethon.objects import InputMediaPhoto, InputMediaVideo
 
 from config import TOKEN, CHAT_ID, USER_ID
 
 PHOTO_URL = "https://bale.ai/_next/image?url=%2Fuploads%2FmainPic.webp&w=1200&q=75"
 VIDEO_URL = "https://dev.bale.ai/sites/default/files/1398-03/final_5ce505a96467ba00144535c8_377089.mp4"
+AUDIO = "https://vgmsite.com/soundtracks/minecraft/abtibsbmth/1-07.%20Haggstrom.mp3"
 
 
 async def test_message_methods(client):
@@ -19,8 +21,18 @@ async def test_message_methods(client):
 
     await sleep(1)
 
-    # response = await client.delete_message(CHAT_ID, message.id)
-    # print(f"delete_message: {response}")
+    response = await client.forward_message(CHAT_ID, CHAT_ID, message.id)
+    print(f"forward_message: {response}")
+
+    await sleep(1)
+
+    response = await client.copy_message(CHAT_ID, CHAT_ID, message.id)
+    print(f"copy_message: {response}")
+
+    await sleep(1)
+
+    response = await client.delete_message(CHAT_ID, message.id)
+    print(f"delete_message: {response}")
 
 
 async def test_update_methods(client):
@@ -29,6 +41,9 @@ async def test_update_methods(client):
 
     response = await client.set_webhook("url")
     print(f"set_webhook: {response}")
+
+    response = await client.get_webhook_info()
+    print(f"get_webhook_info: {response}")
 
     response = await client.delete_webhook()
     print(f"delete_webhook: {response}")
@@ -43,8 +58,8 @@ async def test_attachment_methods(client):
     response = await client.send_photo(CHAT_ID, PHOTO_URL)
     print(f"send_photo: {response}")
 
-    # response = await client.send_audio()
-    # print(f"send_audio: {response}")
+    response = await client.send_audio(CHAT_ID, AUDIO)
+    print(f"send_audio: {response}")
 
     response = await client.send_document(CHAT_ID, PHOTO_URL)
     print(f"send_document: {response}")
@@ -52,35 +67,69 @@ async def test_attachment_methods(client):
     response = await client.send_video(CHAT_ID, VIDEO_URL)
     print(f"send_video: {response}")
 
-    # response = await client.send_voice()
-    # print(f"send_voice: {response}")
+    response = await client.send_voice(CHAT_ID, AUDIO)
+    print(f"send_voice: {response}")
 
     response = await client.send_location(CHAT_ID, latitude=10, longitude=10)
     print(f"send_location: {response}")
 
-    # response = await client.send_contact()
-    # print(f"send_contact: {response}")
+    response = await client.send_contact(chat_id=CHAT_ID, phone_number="09909090999", first_name="name")
+    print(f"send_contact: {response}")
+
+    response = client.send_animation(CHAT_ID, VIDEO_URL)
+    print(f"send_animation: {response}")
+
+    response = client.send_media_group(CHAT_ID, [InputMediaPhoto(PHOTO_URL), InputMediaVideo(VIDEO_URL)])
+    print(f"send_media_group: {response}")
 
     response = await client.get_file(CHAT_ID)
     print(f"get_file: {response}")
 
 
 async def test_chat_methods(client):
+    # response = await client.ban_chat_member(CHAT_ID, USER_ID)
+    # print(f"ban_chat_member: {response}")
+
     response = await client.get_chat(CHAT_ID)
     print(f"get_chat: {response}")
 
     response = await client.get_chat_administrators(CHAT_ID)
     print(f"get_chat_administrators: {response}")
 
-    response = await client.get_chat_members_count(CHAT_ID)
-    print(f"get_chat_members_count: {response}")
-
     response = await client.get_chat_member(CHAT_ID, USER_ID)
     print(f"get_chat_member: {response}")
 
+    response = await client.get_chat_members_count(CHAT_ID)
+    print(f"get_chat_members_count: {response}")
+
+    # response = await client.invite_user(CHAT_ID, USER_ID)
+    # print(f"invite_user: {response}")
+
+    # response = await client.leave_chat(CHAT_ID)
+    # print(f"leave_chat: {response}")
+
+    # response = await client.promote_chat_member(CHAT_ID, USER_ID)
+    # print(f"promote_chat_member: {response}")
+
+    response = await client.send_chat_action(CHAT_ID)
+    print(f"send_chat_action: {response}")
+
+    response = await client.set_chat_photo(CHAT_ID, PHOTO_URL)
+    print(f"set_chat_photo: {response}")
+
+    # response = await client.unban_chat_member(CHAT_ID, USER_ID)
+    # print(f"unban_chat_member: {response}")
+
 
 async def test_payment_methods(client):
-    response = await client.send_invoice(CHAT_ID, "Test", "Test", "Test", "0")
+    response = await client.send_invoice(
+        chat_id=CHAT_ID,
+        title="Test",
+        description="Test",
+        payload="Test",
+        provider_token="Test",
+        prices="0"
+    )
     print(f"send_invoice: {response}")
 
 
