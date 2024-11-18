57ff6c816322160421da245d0ac56affcd6e0d99
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-11-19 02:35:23 +0330

    Added new object-oriented architecture pattern for developing bots

--- balethon/dispatcher/chain.py
+++ balethon/dispatcher/chain.py
@@ -22,6 +22,11 @@ class Chain:
         self.condition = condition
         self.chains = list(chains)
         self.children = []
+        for attribute_name in dir(self):
+            attribute = getattr(self, attribute_name)
+            if isinstance(attribute, EventHandler):
+                attribute.is_method = True
+                self.add_event_handler(attribute)
 
     def to_string(self, keyword="if", tabs=0):
         result = []
@@ -48,49 +53,108 @@ class Chain:
             return True
         return await self.condition(client, event)
 
+    @staticmethod
+    def create_event_handler(event_handler, *args, **kwargs):
+        def decorator(callback):
+            return event_handler(callback, *args, **kwargs)
+        return decorator
+
     def add_event_handler(self, event_handler, *args, **kwargs):
         if isinstance(event_handler, type):
             def decorator(callback):
-                self.add_event_handler(event_handler(callback, *args, **kwargs))
-                return callback
+                event_handler_instance = event_handler(callback, *args, **kwargs)
+                self.add_event_handler(event_handler_instance)
+                return event_handler_instance
             return decorator
         self.children.append(event_handler)
+
+    @classmethod
+    def connect_handler(cls):
+        return cls.create_event_handler(ConnectHandler)
 
     def on_connect(self):
         return self.add_event_handler(ConnectHandler)
 
+    @classmethod
+    def initialize_handler(cls):
+        return cls.create_event_handler(InitializeHandler)
+
     def on_initialize(self):
         return self.add_event_handler(InitializeHandler)
+
+    @classmethod
+    def event_handler(cls, condition=None):
+        return cls.create_event_handler(EventHandler, condition)
 
     def on_event(self, condition=None):
         return self.add_event_handler(EventHandler, condition)
 
+    @classmethod
+    def error_handler(cls, condition=None):
+        return cls.create_event_handler(ErrorHandler, condition)
+
     def on_error(self, condition=None):
         return self.add_event_handler(ErrorHandler, condition)
+
+    @classmethod
+    def update_handler(cls, condition=None):
+        return cls.create_event_handler(UpdateHandler, condition)
 
     def on_update(self, condition=None):
         return self.add_event_handler(UpdateHandler, condition)
 
+    @classmethod
+    def message_handler(cls, condition=None):
+        return cls.create_event_handler(MessageHandler, condition)
+
     def on_message(self, condition=None):
         return self.add_event_handler(MessageHandler, condition)
+
+    @classmethod
+    def edited_message_handler(cls, condition=None):
+        return cls.create_event_handler(EditedMessageHandler, condition)
 
     def on_edited_message(self, condition=None):
         return self.add_event_handler(EditedMessageHandler, condition)
 
+    @classmethod
+    def command_handler(cls, condition=None, name=None, min_arguments=None, max_arguments=None):
+        return cls.create_event_handler(CommandHandler, condition, name, min_arguments, max_arguments)
+
     def on_command(self, condition=None, name=None, min_arguments=None, max_arguments=None):
         return self.add_event_handler(CommandHandler, condition, name, min_arguments, max_arguments)
+
+    @classmethod
+    def callback_query_handler(cls, condition=None):
+        return cls.create_event_handler(CallbackQueryHandler, condition)
 
     def on_callback_query(self, condition=None):
         return self.add_event_handler(CallbackQueryHandler, condition)
 
+    @classmethod
+    def pre_checkout_query_handler(cls, condition=None):
+        return cls.create_event_handler(PreCheckoutQueryHandler, condition)
+
     def on_pre_checkout_query(self, condition=None):
         return self.add_event_handler(PreCheckoutQueryHandler, condition)
+
+    @classmethod
+    def shipping_query_handler(cls, condition=None):
+        return cls.create_event_handler(ShippingQueryHandler, condition)
 
     def on_shipping_query(self, condition=None):
         return self.add_event_handler(ShippingQueryHandler, condition)
 
+    @classmethod
+    def shutdown_handler(cls):
+        return cls.create_event_handler(ShutdownHandler)
+
     def on_shutdown(self):
         return self.add_event_handler(ShutdownHandler)
+
+    @classmethod
+    def disconnect_handler(cls):
+        return cls.create_event_handler(DisconnectHandler)
 
     def on_disconnect(self):
         return self.add_event_handler(DisconnectHandler)
