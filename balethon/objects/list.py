8c6f0789e2cdb44c7327e6d8c299888c3a72bd4e
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2025-02-28 00:50:16 +0330

    Override `bind` method in `List`

--- balethon/objects/list.py
+++ balethon/objects/list.py
@@ -6,12 +6,18 @@ class List(list, Object):
     def wrap(cls, raw_object):
         return cls(raw_object)
 
+    def bind(self, client):
+        self.client = client
+        for element in self:
+            if isinstance(element, Object):
+                element.bind(client)
+
     def unwrap(self):
         return unwrap(self)
 
     def __repr__(self):
         elements = "\n".join(f"{repr(element)}," for element in self)
         if elements:
-            elements = "\n".join(" " * 4 + line for line in elements.splitlines())
+            elements = "\n".join(" "*4 + line for line in elements.splitlines())
             elements = f"\n{elements}\n"
         return f"[{elements}]"
