2a88d83e34c312333bfafcb517f24acc858d02ff
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-05-27 18:53:17 +0330

    Create transaction.py

---
+++ balethon/objects/transaction.py
@@ -0,0 +1,21 @@
+from . import Object
+
+
+class Transaction(Object):
+
+    def __init__(
+            self,
+            id: str = None,
+            status: str = None,
+            user_id: int = None,
+            amount: int = None,
+            created_at: int = None,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.id: str = id
+        self.status: str = status
+        self.user_id: int = user_id
+        self.amount: int = amount
+        self.created_at = created_at
+        
