aab998a47e01c27d59f5721da34cfcc0fc83759a
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-05-29 07:37:03 +0330

    Create answer_callback_query.py

---
+++ balethon/client/alerts/answer_callback_query.py
@@ -0,0 +1,11 @@
+import balethon
+
+
+class AnswerCallbackQuery:
+    async def callback_query(
+        self: "balethon.Client",
+        callback_query_id: str,
+        text: str,
+        show_alert: bool = False,
+    ) -> bool:
+        return await self.auto_execute("post", "answerCallbackQuery", locals())
