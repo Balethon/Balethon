5f0a54c76d7dbd303f6b4628cf3f9eb2d1523ba6
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-05-29 17:13:56 +0330

    feature/answer-pre-checkout-query

--- balethon/objects/pre_checkout_query.py
+++ balethon/objects/pre_checkout_query.py
@@ -25,5 +25,8 @@ class PreCheckoutQuery(Object):
         self.shipping_option_id: str = shipping_option_id
         self.order_info: "objects.OrderInfo" = order_info
 
+    async def answer(self, ok: bool = True, error_message: str = None) -> bool:
+        return await self.client.answer_pre_checkout_query(self.id, ok, error_message)
+
     async def inquire(self) -> "objects.Transaction":
         return await self.client.inquire_transaction(self.id)
