8a6901b49a87b88441539d499a7616fac1c50390
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-05-29 16:45:29 +0330

    feature/answer-callback-query

--- balethon/objects/callback_query.py
+++ balethon/objects/callback_query.py
@@ -5,20 +5,18 @@ from ..sync_support import add_sync_support_to_object
 
 @add_sync_support_to_object
 class CallbackQuery(Object):
-    attribute_names = [
-        ("author", "from")
-    ]
+    attribute_names = [("author", "from")]
 
     def __init__(
-            self,
-            id: str = None,
-            author: "objects.User" = None,
-            message: "objects.Message" = None,
-            inline_message_id: str = None,
-            chat_instance: str = None,
-            data: str = None,
-            game_short_name: str = None,
-            **kwargs
+        self,
+        id: str = None,
+        author: "objects.User" = None,
+        message: "objects.Message" = None,
+        inline_message_id: str = None,
+        chat_instance: str = None,
+        data: str = None,
+        game_short_name: str = None,
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self.id: str = id
@@ -29,9 +27,5 @@ class CallbackQuery(Object):
         self.data: str = data
         self.game_short_name: str = game_short_name
 
-    async def answer(
-            self,
-            text: str,
-            reply_markup: "objects.ReplyMarkup" = None
-    ):
-        return await self.client.send_message(self.message.chat.id, text, reply_markup)
+    async def answer(self, text: str = None, show_alert: bool = None) -> bool:
+        return await self.client.asnwer_callback_query(self.id, text, show_alert)
