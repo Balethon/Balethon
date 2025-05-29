f59072d06d737837aa1752f3d136c79c5defb5f5
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-05-29 08:44:49 +0330

    updated

--- balethon/client/payments/answer_pre_checkout_query.py
+++ balethon/client/payments/answer_pre_checkout_query.py
@@ -5,7 +5,7 @@ class AnswerPreCheckoutQuery:
     async def answer_pre_checkout_query(
         self: "balethon.Client",
         pre_checkout_query_id: str,
-        ok: bool,
+        ok: bool = True,
         error_message: str = None,
     ) -> bool:
         return await self.auto_execute("post", "answerPreCheckoutQuery", locals())
