621967e3ebdf061b2dd6c05f10758c4ba3e19996
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-01 22:19:39 +0330

    Added `Transaction` object

---
+++ balethon/objects/transaction.py
@@ -0,0 +1,25 @@
+from . import Object
+
+
+class Transaction(Object):
+    attribute_names = [
+        ("user_id", "userID"),
+        ("created_at", "createdAt"),
+    ]
+
+    def __init__(
+        self,
+        id: str = None,
+        status: str = None,
+        user_id: int = None,
+        amount: int = None,
+        created_at: int = None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.id: str = id
+        self.status: str = status
+        self.user_id: int = user_id
+        self.amount: int = amount
+        self.created_at = created_at
+      
