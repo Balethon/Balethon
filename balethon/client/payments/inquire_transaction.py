d74768b1828ab4c9ee13003f6899c03f5b913ac8
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-01 20:15:23 +0330

    Add `inquireTransaction` method

---
+++ balethon/client/payments/inquire_transaction.py
@@ -0,0 +1,11 @@
+import balethon
+from ...objects import Transaction
+
+
+class InquireTransaction:
+  
+    async def inquire_transaction(
+        self: "balethon.Client", transaction_id: str
+    ) -> Transaction:
+        return await self.auto_execute("post", "inquireTransaction", locals())
+      
