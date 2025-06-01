51eb562429eac10e38cb49c68d010d70668c9bcb
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-01 23:40:02 +0330

    Fix `ChatAction` members

--- balethon/enums/chat_action.py
+++ balethon/enums/chat_action.py
@@ -5,9 +5,6 @@ from .name_enum import NameEnum
 
 class ChatAction(NameEnum):
     TYPING = auto()
-    UPLOAD_DOCUMENT = auto()
     UPLOAD_PHOTO = auto()
-    UPLOAD_VOICE = auto()
     UPLOAD_VIDEO = auto()
     RECORD_VOICE = auto()
-    
