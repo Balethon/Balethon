9b5126806f7848e28bde46dfd9252100c1dcde8a
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-01 23:43:14 +0330

    Updated format file

--- balethon/client/payments/inquire_transaction.py
+++ balethon/client/payments/inquire_transaction.py
@@ -5,7 +5,7 @@ from ...objects import Transaction
 class InquireTransaction:
     
     async def inquire_transaction(
-        self: "balethon.Client", transaction_id: str
+        self: "balethon.Client",
+        transaction_id: str
     ) -> Transaction:
         return await self.auto_execute("post", "inquireTransaction", locals())
-      
