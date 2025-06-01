104a0c4c40a650b229b3c38ca3cc0be2d9b2c81b
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-01 18:36:28 +0330

    fix: resolve `ChatType` enum issue in `get_chat` method

--- balethon/client/chats/get_chat.py
+++ balethon/client/chats/get_chat.py
@@ -7,14 +7,32 @@ from ...enums import ChatType
 
 
 class GetChat:
+    
+    async def get_chat(self: "balethon.Client", chat_id: Union[int, str]) -> Chat:
+        result = Chat()
 
-    async def get_chat(
-            self: "balethon.Client",
-            chat_id: Union[int, str]
-    ) -> Chat:
         # 1234567890 | "1234567890"
-        if isinstance(chat_id, int) or (isinstance(chat_id, str) and chat_id.isnumeric()):
-            return await self.auto_execute("get", "getChat", locals())
+        if isinstance(chat_id, int) or (
+            isinstance(chat_id, str) and chat_id.isnumeric()
+        ):
+            info = await self.auto_execute("get", "getChat", locals())
+            result.id = info.id
+            result.username = info.username
+            result.description = info.description
+            
+            if info.type == "private":
+                result.type = ChatType.PRIVATE
+                result.first_name = info.first_name
+                result.bind(self)
+                return result
+            
+            result.title = info.title
+            if info.type == "group":
+                result.type = ChatType.GROUP
+            if info.type == "channel":
+                result.type = ChatType.CHANNEL
+            result.bind(self)
+            return result
 
         # "@username"
         if chat_id.startswith("@"):
@@ -34,8 +52,6 @@ class GetChat:
 
         info = await self.connection.get_peer_info(chat_id)
 
-        result = Chat()
-
         if info["query"].get("token"):
             token = info["query"]["token"]
             result.invite_link = f"{self.connection.SHORT_URL}/join/{token}"
@@ -43,7 +59,9 @@ class GetChat:
         info = info["props"]["pageProps"]
 
         if info["peer"]["type"] == 0:
-            raise RPCError.create(code=404, description="no such group or user", reason="getChat")
+            raise RPCError.create(
+                code=404, description="no such group or user", reason="getChat"
+            )
 
         elif info["peer"]["type"] == 1:
             result.type = ChatType.PRIVATE
@@ -52,11 +70,16 @@ class GetChat:
             result.description = info["user"]["description"]
 
         elif info["peer"]["type"] == 2:
-            result.type = ChatType.CHANNEL if info["group"]["isChannel"] else ChatType.GROUP
-            result.username = info["group"]["nick"] if info["group"].get("nick") else None
+            result.type = (
+                ChatType.CHANNEL if info["group"]["isChannel"] else ChatType.GROUP
+            )
+            result.username = (
+                info["group"]["nick"] if info["group"].get("nick") else None
+            )
             result.title = info["group"]["title"]
             result.description = info["group"]["description"]
 
         result.id = info["peer"]["id"]
         result.bind(self)
         return result
+        
