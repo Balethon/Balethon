a092fbf8f4fdd5456640f643a2c6b1b7ca8d1d38
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-10-24 14:29:39 +0330

    Refactor `send_chat_action` method to use `ChatAction` enum and expand `ChatAction` enum with additional actions

--- balethon/client/chats/send_chat_action.py
+++ balethon/client/chats/send_chat_action.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import balethon
+from balethon import enums
 
 
 class SendChatAction:
@@ -8,7 +9,7 @@ class SendChatAction:
     async def send_chat_action(
             self: "balethon.Client",
             chat_id: Union[int, str],
-            action: str = "typing"
+            action: "enums.ChatAction"
     ) -> bool:
         chat_id = await self.resolve_peer_id(chat_id)
         chat_id = str(chat_id)  # The sendChatAction method only works with a string chat_id
--- balethon/enums/chat_action.py
+++ balethon/enums/chat_action.py
@@ -5,3 +5,9 @@ from .name_enum import NameEnum
 
 class ChatAction(NameEnum):
     TYPING = auto()
+    UPLOAD_PHOTO = auto()
+    RECORD_VIDEO = auto()
+    UPLOAD_VIDEO = auto()
+    CHOOSE_STICKER = auto()
+    UPLOAD_VOICE = auto()
+    RECORD_VOICE = auto()
