1fd354591ecd2f32f0aa1a112781e1ecb84675dc
BalethonPlus <balethonplus@gmail.com>
2025-10-30 10:36:59 +0330

    Set default action to `TYPING` in `send_chat_action` method

--- balethon/client/chats/send_chat_action.py
+++ balethon/client/chats/send_chat_action.py
@@ -9,7 +9,7 @@ class SendChatAction:
     async def send_chat_action(
             self: "balethon.Client",
             chat_id: Union[int, str],
-            action: "enums.ChatAction"
+            action: "enums.ChatAction" = enums.ChatAction.TYPING
     ) -> bool:
         chat_id = await self.resolve_peer_id(chat_id)
         chat_id = str(chat_id)  # The sendChatAction method only works with a string chat_id
