4fc37557a2338c63b430c4bfeb7994b70c59aac9
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-11-14 20:22:02 +0330

    Add `Client.create_invoice_link()` method

--- balethon/client/payments/__init__.py
+++ balethon/client/payments/__init__.py
@@ -1,7 +1,13 @@
 from .answer_pre_checkout_query import AnswerPreCheckoutQuery
+from .create_invoice_link import CreateInvoiceLink
 from .inquire_transacion import InquireTransaction
 from .send_invoice import SendInvoice
 
 
-class Payments(AnswerPreCheckoutQuery, InquireTransaction, SendInvoice):
+class Payments(
+    AnswerPreCheckoutQuery,
+    CreateInvoiceLink,
+    InquireTransaction,
+    SendInvoice
+):
     pass
