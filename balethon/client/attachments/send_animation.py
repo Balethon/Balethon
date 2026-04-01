0ac9c02f61b5ee7274208ed3327f57c2541581c1
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2026-04-01 18:07:54 +0330

    Add userbot support for `send_video` and `send_animation`

--- balethon/client/attachments/send_animation.py
+++ balethon/client/attachments/send_animation.py
@@ -2,6 +2,7 @@ from typing import Union, BinaryIO
 
 import balethon
 from ...objects import InputMedia, resolve_media, Message, ReplyMarkup
+from ...proto import request_pb2, struct_pb2
 
 
 class SendAnimation:
@@ -17,8 +18,45 @@ class SendAnimation:
             reply_markup: ReplyMarkup = None,
             reply_to_message_id: int = None
     ) -> Message:
-        chat_id = await self.resolve_peer_id(chat_id)
         animation = resolve_media(animation)
+
+        if self.is_userbot():
+            peer_id, peer_type = map(int, chat_id.split("|"))
+            file = await self.upload_file(chat_id, animation, struct_pb2.SEND_TYPE_GIF)
+            return await self.invoke(
+                service_name="bale.messaging.v2.Messaging",
+                method="SendMessage",
+                payload=request_pb2.SendMessage(
+                    peer=struct_pb2.Peer(
+                        type=peer_type,
+                        id=peer_id
+                    ),
+                    rid=self.ws_connection.create_rid(),
+                    message=struct_pb2.Message(
+                        document_message=struct_pb2.DocumentMessage(
+                            file_id=file.id,
+                            access_hash=peer_id,
+                            file_size=file.size,
+                            name=file.name,
+                            mime_type=file.mime_type,
+                            ext=struct_pb2.DocumentEx(
+                                document_ex_gif=struct_pb2.DocumentExGif(
+                                    w=100,
+                                    h=100,
+                                    duration=10
+                                )
+                            ),
+                            caption=struct_pb2.TextMessage(text=caption)
+                        )
+                    ),
+                    ex_peer=struct_pb2.Peer(
+                        type=peer_type,
+                        id=peer_id
+                    )
+                )
+            )
+
+        chat_id = await self.resolve_peer_id(chat_id)
         return await self.auto_execute(
             "sendAnimation",
             locals(),
--- balethon/client/attachments/send_video.py
+++ balethon/client/attachments/send_video.py
@@ -2,6 +2,7 @@ from typing import Union, BinaryIO
 
 import balethon
 from ...objects import InputMedia, resolve_media, Message, ReplyMarkup
+from ...proto import request_pb2, struct_pb2
 
 
 class SendVideo:
@@ -17,6 +18,43 @@ class SendVideo:
             reply_markup: ReplyMarkup = None,
             reply_to_message_id: int = None
     ) -> Message:
-        chat_id = await self.resolve_peer_id(chat_id)
         video = resolve_media(video)
+
+        if self.is_userbot():
+            peer_id, peer_type = map(int, chat_id.split("|"))
+            file = await self.upload_file(chat_id, video, struct_pb2.SEND_TYPE_VIDEO)
+            return await self.invoke(
+                service_name="bale.messaging.v2.Messaging",
+                method="SendMessage",
+                payload=request_pb2.SendMessage(
+                    peer=struct_pb2.Peer(
+                        type=peer_type,
+                        id=peer_id
+                    ),
+                    rid=self.ws_connection.create_rid(),
+                    message=struct_pb2.Message(
+                        document_message=struct_pb2.DocumentMessage(
+                            file_id=file.id,
+                            access_hash=peer_id,
+                            file_size=file.size,
+                            name=file.name,
+                            mime_type=file.mime_type,
+                            ext=struct_pb2.DocumentEx(
+                                document_ex_video=struct_pb2.DocumentExVideo(
+                                    w=100,
+                                    h=100,
+                                    duration=10
+                                )
+                            ),
+                            caption=struct_pb2.TextMessage(text=caption)
+                        )
+                    ),
+                    ex_peer=struct_pb2.Peer(
+                        type=peer_type,
+                        id=peer_id
+                    )
+                )
+            )
+
+        chat_id = await self.resolve_peer_id(chat_id)
         return await self.auto_execute("sendVideo", locals())
