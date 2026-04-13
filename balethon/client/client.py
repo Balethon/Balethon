606c3409eca4d8043131d99efeab7937ec9bcfbe
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2026-04-13 23:44:56 +0330

    Add support for validating password in `auth` method

--- balethon/client/client.py
+++ balethon/client/client.py
@@ -240,11 +240,14 @@ class Client(Chain, Messages, Updates, Users, Attachments, Chats, InviteLinks, P
             try:
                 auth = await self.validate_code(sent_code.transaction_hash, input("Enter phone code: "))
             except RPCError as error:
-                if error.description == "PHONE_NUMBER_UNOCCUPIED":
+                if error.code == 5:
                     auth = await self.sign_up(sent_code.transaction_hash, input("Enter a name for your account: "))
                     break
-                elif error.description == "PHONE_CODE_INVALID":
+                elif error.code == 3:
                     print("The phone code is invalid, try again")
+                elif error.code == 4:
+                    auth = await self.validate_password(sent_code.transaction_hash, input("Enter your password: "))
+                    break
                 else:
                     raise error
             else:
